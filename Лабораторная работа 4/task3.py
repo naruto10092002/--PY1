65e33095702b3345609e1655b7dde7a35335cb48
naruto10092002 <115168586+naruto10092002@users.noreply.github.com>
2022-11-13 15:15:26 +0300

    Update task3.py

--- Лабораторная работа 4/task3.py
+++ Лабораторная работа 4/task3.py
@@ -1,10 +1,9 @@
-def delete(list_, index=None):
+def delete(list_, index=None):                          # Аналогично предыдущему: оставил копирование списка
     if index is None:
         index = len(list_) - 1                          # Аргументом по умолчанию выбираем индекс последнего элемента
     list_ = list_[:]                                    # Копируем список
-    result = list_[:index] + list_[index + 1:]          # Соединяем два среза без удаляемого элемента
 
-    return result
+    return list_[:index] + list_[index + 1:]            # Соединяем два среза без удаляемого элемента и возвращаем
 
 
 print(delete([0, 0, 1, 2], index=0))  # [0, 1]
